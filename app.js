88af772d98286c767e0d2e3f5997ec6bbe6efa39
JSaiyan <183049245+JSaiyan@users.noreply.github.com>
2025-01-30 14:59:58 -0800

    done with ppappointmentScheduler

--- app.js
+++ app.js
@@ -4,20 +4,44 @@ import express from 'express';
 // instantiate an express application
 const app = express();
 
+//
+const appointments= [];
+
 // serve static files from the 'public' directory
 app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}));
 
 // define the port number where our server will listen
 const PORT = 3000;
 
 
 // create a "default" route for our website's home page
-app.get('/', (req, res) => {
+//app.get('/', (req, res) => {
 
   // send the home page as a response to the client
-  res.sendFile(`${import.meta.dirname}/views/home.html`);
-});
+  //res.send('Hello World');
+//});
 
+//get 
+app.get('/', (req,res) => 
+  {
+      //Send our home page as a response to the client
+      res.sendFile(`${import.meta.dirname}/views/home.html`);
+  });
+
+  //post
+  app.post('/submit-order', (req, res) =>
+    {
+        console.log(req.body);
+        appointments.push(req.body)
+        res.send(`<h1>your appointment is confirmed!</h1>`);
+    });
+
+    app.get('/admin/appointments', (req, res) =>
+      {
+          res.send(appointments);
+      });
 
 
 // Tell the server to listen on our specified port
